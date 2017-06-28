ade563bf0136e3287de24949a5c07b34187f4211
14EC002 <govindhpavi@gmail.com>
2017-06-28 12:19:16 +0530

    Create evenbetween.java

---
+++ evenbetween.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+import java.util.*;
+
+class evenbetween{
+public static void main(String[] args){
+int n1,n2;
+Scanner in=new Scanner(System.in);
+n1=in.nextInt();
+Scanner s=new Scanner(System.in);
+n2=s.nextInt();
+for(int i=n1;i<n2;i++)
+{
+if(i%2==0)
+{
+System.out.println(i+);
+}
+}
+}}
