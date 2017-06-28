b5fba8d161f3d6e88d3b77f481e68d30e91eb4e0
14EC002 <govindhpavi@gmail.com>
2017-06-28 10:34:21 +0530

    Create alphabet.java

---
+++ alphabet.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+import java.util.*;
+
+class alphabet{
+
+ public static void main(String[] args) {
+char n;
+
+  Scanner s = new Scanner(System.in);
+       n = s.next().charAt(0);
+if((n>'a' && n<'z'))
+
+{
+
+System.out.println("alphabet");
+
+}
+
+else
+
+{
+
+System.out.println("not an alphabet");
+
+}
+
+}
+}
