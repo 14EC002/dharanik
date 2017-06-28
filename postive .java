bedd82aad7f8666b9e41391bfbbb21cf3ddd0da4
14EC002 <govindhpavi@gmail.com>
2017-06-28 09:56:54 +0530

    Create postive .java

---
+++ postive .java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+class postive{
+ public static void main(String[] args)
+int n;
+ Scanner s = new Scanner(System.in);
+        n = s.nextInt();
+if(n<0)
+{
+System.out.println("negative");
+}
+else if(n==0)
+{
+System.out.println("zero");
+}
+else
+{
+System.out.println("postive");
+}
+}}
